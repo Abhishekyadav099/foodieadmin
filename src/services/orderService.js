0d0bddb95e9201af700e7bb80112d1a4552460e5
Abhishek Yadav <abhishekyadavv9990@gmail.com>
2025-11-29 15:56:59 +0530

    init

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/orders";
+const API_URL = "https://foodiesspringboot.onrender.com/api/orders";
 
 export const fetchAllOrders = async () => {
-    try {
-        const response = await axios.get(API_URL+"/all");
-        return response.data;
-    } catch (error) {
-        console.error('Error occured while fetching the orders', error);
-        throw error;
-    }
-}
+  try {
+    const response = await axios.get(API_URL + "/all");
+    return response.data;
+  } catch (error) {
+    console.error("Error occured while fetching the orders", error);
+    throw error;
+  }
+};
 
 export const updateOrderStatus = async (orderId, status) => {
-    try {
-        const response = await axios.patch(
-            `${API_URL}/status/${orderId}?status=${status}`
-        );
-        return response.status === 200;
-    } catch (error) {
-        console.error('Error occured while updating the status', error);
-        throw error;
-    }
-}
+  try {
+    const response = await axios.patch(
+      `${API_URL}/status/${orderId}?status=${status}`
+    );
+    return response.status === 200;
+  } catch (error) {
+    console.error("Error occured while updating the status", error);
+    throw error;
+  }
+};
